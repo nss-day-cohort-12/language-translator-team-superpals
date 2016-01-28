9c367b46019efd63cc226f1b8fc0ef87ab8af10d
Paloma Venegas Stephens <palomavenegasstephens@Palomas-MacBook-Pro.local>
2016-01-28 10:47:05 -0600

    first

--- Spanish.js
+++ Spanish.js
@@ -59,6 +59,43 @@ function getInfo() {
 	console.log(Translator.getGerman(phrase));
 };
 
+// IIFE
+// Immediately invoked function expression
+var Sedan = (function() {
+  var color = "blue";
+  var type = "sedan";
+
+  return {
+    manufacture_date: new Date(),
+    getColor: function() {
+      return color;
+    },
+    setColor: function(newColor) {
+      color = newColor;
+    },
+    getType: function() {
+      return type;
+    }
+  };
+})();
+
+/* 
+  Now, code that uses this module can call methods, and access
+  properties that are in the public interface, but cannot change
+  any private variables.
+*/
+Sedan.setColor("green");  // this works
+
+/*
+ This adds the property to the public interface, but the 
+ existing, internal type property remains unchanged.
+*/
+Sedan.type = "minivan"; 
+console.log(Sedan.getType());
+
+
+
+
 
 
 
